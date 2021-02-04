8e93023a0ffdae178b1f44294af3e561d0a6e351
vyankateshsmoti <pranavmoti25@gmail.com>
2021-02-04 07:16:30 +0530

    Platform added by vyankatesh

--- sketch.js
+++ sketch.js
@@ -5,6 +5,7 @@ const Bodies = Matter.Bodies;
 var engine, world;
 var box1, pig1;
 var backgroundImg;
+var platform;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -17,6 +18,8 @@ function setup(){
 
     
     ground = new Ground(600,height,1200,20)
+    
+    platform = new Ground(150,305,300,170);
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -43,9 +46,11 @@ function draw(){
     console.log(box2.body.position.x);
     console.log(box2.body.position.y);
     console.log(box2.body.angle);
+
     box1.display();
     box2.display();
     ground.display();
+    platform.display();
     pig1.display();
     log1.display();
 
